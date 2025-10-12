ebd06a514ee5b4334ce496f8890a2fba5831179f
smashsmashin <64376264+smashsmashin@users.noreply.github.com>
2025-10-12 17:16:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // Replace these placeholder values with your actual Google API Key and Client ID.
-const GOOGLE_API_KEY = 'YOUR_API_KEY';
-const GOOGLE_CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com';
+const GOOGLE_API_KEY = 'AIzaSyApsB1-wDGrT6wsL5cV1vv99kLWayV-1og';
+const GOOGLE_CLIENT_ID = '414398931859-h04be241u8er4lgiun26qpavdjrnqecc.apps.googleusercontent.com';
